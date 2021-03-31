32c61a245c5b8d1589e76b5484f35047c2deb24b
hector <nandohector1@gmail.com>
2021-03-31 11:26:56 -0600

    CreateTask and disabled arrows

--- src/app/kanbanBoard/kanbanBoard.component.ts
+++ src/app/kanbanBoard/kanbanBoard.component.ts
@@ -1,17 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { getElement } from 'h8k-components/dist/types/stencil-public-runtime';
 
 @Component({
   selector: 'kanban-board',
   templateUrl: './kanbanBoard.component.html',
   styleUrls: ['./kanbanBoard.component.scss']
 })
+
 export class KanbanBoard implements OnInit {
   tasks: Task[];
   stagesNames: string[];
   stagesTasks: any[]; //Only used for rendering purpose
 
   ngOnInit() {
-    // Each task is uniquely identified by its name. 
+    // Each task is uniquely identified by its name.
     // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
     this.tasks = [
       { name: '0', stage: 0 },
@@ -20,7 +22,7 @@ export class KanbanBoard implements OnInit {
     this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
     this.configureTasksForRendering();
   }
-  
+
   // this function has to be called whenever tasks array is changed to construct stagesTasks for rendering purpose
   configureTasksForRendering = () => {
     this.stagesTasks = [];
@@ -35,6 +37,23 @@ export class KanbanBoard implements OnInit {
 
   generateTestId = (name) => {
     return name.split(' ').join('-');
+  }
+
+  @ViewChild("create-task-input") taskName: ElementRef;
+
+  createTask = (nombre) => {
+    console.log("Hola");
+    this.tasks.push({ name: nombre, stage: 0 });
+    this.configureTasksForRendering();
+  }
+  taskBack = (task: Task) => {
+    console.log(task.name);
+  }
+  taskFoward = (task: Task) => {
+    console.log(task.name);
+  }
+  taskDelete = (task: Task) => {
+    console.log(task.name);
   }
 }
 
